3828f64ebfb62c61850e66f6a607b74fa3d1e9fb
Matheus da Mata <matheusdamatag@gmail.com>
2022-09-22 19:34:42 -0300

    CRUD complet + CRUD Phrase random extra

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { Sequelize } from 'sequelize';
 
 import { Phrase } from '../models/Phrase';
 
@@ -14,4 +15,65 @@ export const random = (req: Request, res: Response) => {
 export const nome = (req: Request, res: Response) => {
     let name: string = req.params.name;
     res.json({ nome: name })
+}
+
+export const createPhrase = async (req: Request, res: Response) => {
+    let { author, txt } = req.body;
+
+    let newPhrase = await Phrase.create({ author, txt });
+
+    res.status(201);
+    res.json({ id: newPhrase.id, author, txt });
+}
+
+export const listPhrases = async (req: Request, res: Response) => {
+    let list = await Phrase.findAll();
+    res.json({ list });
+}
+
+export const getPhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+
+    let phrase = await Phrase.findByPk(id);
+    if(phrase) {
+        res.json({ phrase });
+    } else {
+        res.json ({error: 'Phrase not found!'});
+    }  
+}
+
+export const updatePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    let { author, txt } = req.body;
+
+    let phrase = await Phrase.findByPk(id);
+    if(phrase) {
+        phrase.author = author;
+        phrase.txt = txt;
+        await phrase.save();
+
+        res.json({ phrase });
+    } else {
+        res.json({ error: 'Phrase not found!' });
+    }
+}
+
+export const deletePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    await Phrase.destroy({ where: { id } });
+    res.json({});
+}
+
+export const randomPhrase = async (req: Request, res: Response) => {
+    let phrase = await Phrase.findOne({
+        order: [
+            Sequelize.fn('RAND')
+        ]
+    });
+
+    if(phrase) {
+        res.json({ phrase });
+    } else {
+        res.json({ error: 'Phrases not found!' })
+    }
 }
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -8,4 +8,11 @@ router.get('/ping', ApiController.ping);
 router.get('/random', ApiController.random);
 router.get('/nome/:name', ApiController.nome);
 
+router.post('/phrase', ApiController.createPhrase);
+router.get('/phrases', ApiController.listPhrases);
+router.get('/phrase/random', ApiController.randomPhrase);
+router.get('/phrase/:id', ApiController.getPhrase);
+router.put('/phrase/:id', ApiController.updatePhrase);
+router.delete('/phrase/:id', ApiController.deletePhrase);
+
 export default router;
